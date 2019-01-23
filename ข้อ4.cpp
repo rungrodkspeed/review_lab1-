2b3a0527108a30e462a5242e19883819e230447d
unknown <rungrodks@hotmail.com>
2019-01-24 00:50:04 +0700

    0:50AM

---
+++ ข้อ4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+    cout << "Please input year of birth:";
+    cin >> year;
+    cout << "Your age " << 2019-year << " years old";
+    return 0;
+}
